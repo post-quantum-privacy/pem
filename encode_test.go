d99c5ae6f2d195ba4bcb542be70dbd1d4d24c7d1
go-compile <97609133+go-compile@users.noreply.github.com>
2023-10-14 15:10:55 +0100

    feat: add encode test

--- encode_test.go
+++ encode_test.go
@@ -5,17 +5,22 @@ import (
 	"fmt"
 	"testing"
 
+	stdPem "encoding/pem"
+
 	"github.com/post-quantum-privacy/pem"
 )
 
 func TestEncode(t *testing.T) {
-	buf := bytes.NewBufferString(`MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGZNv6T7dNmLSyrC6j9C5UMKQ/Sf
+	data := `MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGZNv6T7dNmLSyrC6j9C5UMKQ/Sf
 rayfBzM9mhNHzV/tXBqCnTL4lQ2fHarAhVbJ2fP2nXXJWgjP1L5OxHXmcZaUUWU9
 P/0cv6BZAvx4sH/CN0o+gxWgV0PTZ9tMvf94XHNc157qi9grPRkahhsv7ujRAEJ2
-D6CIPBoHkmZKQlxjAgMBAAE=`)
+D6CIPBoHkmZKQlxjAgMBAAE=`
+
+	buf := bytes.NewBufferString(data)
 
 	output := bytes.NewBuffer(nil)
-	n, err := pem.Encode("RSA PUBLIC KEY", buf, output, nil)
+	kind := "RSA PUBLIC KEY"
+	n, err := pem.Encode(kind, buf, output, nil)
 	if err != nil {
 		t.Error(err)
 	}
@@ -25,4 +30,18 @@ D6CIPBoHkmZKQlxjAgMBAAE=`)
 	}
 
 	fmt.Println(output.String())
+
+	p, rest := stdPem.Decode(output.Bytes())
+	if len(rest) != 0 {
+		t.Errorf("rest is %d, should be zero", len(rest))
+	}
+
+	// they do equal, likely a issue with carrige returns
+	if !bytes.Equal([]byte(data), p.Bytes) {
+		t.Error("data bytes do not match")
+	}
+
+	if p.Type != kind {
+		t.Error("kind/type does not match")
+	}
 }
