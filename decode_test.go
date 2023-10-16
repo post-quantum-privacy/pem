70f73fdd738efda76e39a1cbb1be4b484880254e
go-compile <97609133+go-compile@users.noreply.github.com>
2023-10-16 16:32:09 +0100

    feat: decode headers unit test

--- decode_test.go
+++ decode_test.go
@@ -22,11 +22,43 @@ func TestDecode(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if headers != nil {
-		t.Error("headers should be nil")
+	if len(headers) > 0 {
+		t.Error("headers should be empty")
 	}
 
 	if kind != "RSA PRIVATE KEY" {
+		t.Error("kind is incorrect")
+	}
+
+	if !bytes.Equal(data, body.Bytes()) {
+		t.Error("bytes do not match")
+	}
+}
+
+func TestDecodeHeaders(t *testing.T) {
+	data := []byte("some secret key here")
+	out := bytes.NewBuffer(nil)
+	headers := make(map[string]string)
+	headers["hash"] = "SHA256"
+	headers["type"] = "ecdsa"
+	headers["key"] = "X448"
+
+	_, err := pem.Encode("PGP MESSAGE", bytes.NewBuffer(data), out, headers)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body := bytes.NewBuffer(nil)
+	kind, headers, err := pem.Decode(out, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(headers) != 3 {
+		t.Error("headers should equal 3")
+	}
+
+	if kind != "PGP MESSAGE" {
 		t.Error("kind is incorrect")
 	}
 
